0c9e5fc5588e8012934ecf3911a6ef1389a2522c
Sabri <MrSherlockHolmes123@gmail.com>
2016-08-16 18:46:50 +0100

    Update script-jacker-hacker.js

--- script-jacker-hacker.js
+++ script-jacker-hacker.js
@@ -1,6 +1,6 @@
 xhttp = new XMLHttpRequest();
 for (i = 0; i++; i < document.getElementsByTagName("script").length) {
-  xhttp.open("GET", document.getElementsByTagName("script")[i].src, true);
+  xhttp.open("GET", document.getElementsByTagName("script")[i].src, false);
   xhttp.send();
   console.log(xhttp.responseText);
 }
