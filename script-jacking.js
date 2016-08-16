c99c698c251b218723041f3e634945267f5c6d91
Sabri <MrSherlockHolmes123@gmail.com>
2016-08-16 17:05:19 +0100

    Update script-jacking.js

--- script-jacking.js
+++ script-jacking.js
@@ -1,3 +1,3 @@
 /* this is an evil script-jacking page!!!!! HHAHAHAHAHAHAHHA */
 
-document.getElementsByTagName("body")[0].innerHTML = "This page has been <b size=\"6\">hacked</b>!!!";
+document.getElementsByTagName("body")[0].innerHTML = "This page has been <b size=\"6\">hacked<\/b>!!!";
