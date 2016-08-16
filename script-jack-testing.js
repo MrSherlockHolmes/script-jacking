ff7cfff935e93a676bd29e42d46a3fff71aaa6e8
Sabri <MrSherlockHolmes123@gmail.com>
2016-08-16 19:32:18 +0100

    Create script-jack-testing.js

---
+++ script-jack-testing.js
@@ -0,0 +1,17 @@
+/* takes an array of scripts urls, and checks for unusual stuff */
+
+var xhttp = new XMLHttpRequest();
+var reject = false;
+urls = ["https://mrsherlockholmes.github.io/script-jacking/script-jacking.js"];
+
+for (url in urls) {
+  xhttp.open("GET", url, false);
+  xhttp.send();
+  statements = xhttp.responseText.split(";");
+  for (statement in statements) {
+    stm = statement.trim();
+    if (stm.substr(0, 19) === "document.getElement") {
+      reject = true;
+    }
+  }
+}
